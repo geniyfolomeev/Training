3fcc5e512793a49c57fdf44e878885545bd5e0a6
geniyfolomeev <geniyfolomeev@gmail.com>
2022-03-06 19:38:36 +0400

    Script now visits python.org

--- script.py
+++ script.py
@@ -1,8 +1,15 @@
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
-driver.get('https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python')
+driver.maximize_window()
+driver.get('https://www.python.org/')
+driver.find_element(By.CSS_SELECTOR, '#id-search-field').send_keys("selenium")
+driver.find_element(By.CSS_SELECTOR, '#id-search-field').send_keys(Keys.ENTER)
+time.sleep(5)
 driver.quit()
